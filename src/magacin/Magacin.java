7066aa38c9a2825477d075251ef5e877db09ef6a
Marko Radunovic <markoradunovic998@gmail.com>
2021-03-14 23:14:27 +0100

    Metode za dodavanje artikala unutar klase Magacin implementirane na drugaciji nacin

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -13,8 +13,19 @@ public class Magacin implements MagacinInterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		if(knjiga!= null && !knjige.contains(knjiga)) {
+			knjige.add(knjiga);
+		}else if(knjiga!=null && knjige.contains(knjiga)) {
+			int ind = knjige.indexOf(knjiga);
+			int kol = knjige.get(ind).getKolicina();
+			try {
+				knjige.get(ind).setKolicina(kol+ knjiga.getKolicina());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		throw new RuntimeException("Ne moze se dodati knjiga koja je null!");
 	}
 
 	@Override
@@ -31,8 +42,20 @@ public class Magacin implements MagacinInterfejs {
 
 	@Override
 	public void dodajKancelarijskiMaterijal(KancelarijskiMaterijal materijal) {
-		// TODO Auto-generated method stub
 
+		if(materijal!= null && !kancelarijskiMaterijali.contains(materijal)) {
+			kancelarijskiMaterijali.add(materijal);
+		}else if(materijal!=null && kancelarijskiMaterijali.contains(materijal)) {
+			int ind = kancelarijskiMaterijali.indexOf(materijal);
+			int kol = kancelarijskiMaterijali.get(ind).getKolicina();
+			try {
+				kancelarijskiMaterijali.get(ind).setKolicina(kol+ materijal.getKolicina());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		throw new RuntimeException("Ne moze se dodati kancelarijski materijal koji je null!");
 	}
 
 	@Override
@@ -49,8 +72,20 @@ public class Magacin implements MagacinInterfejs {
 
 	@Override
 	public void dodajKucnuHemiju(KucnaHemija hemija) {
-		// TODO Auto-generated method stub
 
+		if(hemija!= null && !kucneHemije.contains(hemija)) {
+			kucneHemije.add(hemija);
+		}else if(hemija!=null && kucneHemije.contains(hemija)) {
+			int ind = kucneHemije.indexOf(hemija);
+			int kol = kucneHemije.get(ind).getKolicina();
+			try {
+				kucneHemije.get(ind).setKolicina(kol+ hemija.getKolicina());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		throw new RuntimeException("Ne moze se dodati kucna hemija koja je null!");
 	}
 
 	@Override
