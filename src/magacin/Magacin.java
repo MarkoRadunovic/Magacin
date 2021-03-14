76d25b7b93d0c1f99c817111036626d38ddd34a3
Marko Radunovic <markoradunovic998@gmail.com>
2021-03-14 22:59:31 +0100

    U klasi Magacin su do kraja implementirane sve metode iz interfejsa

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -13,55 +13,164 @@ public class Magacin implements MagacinInterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
 
+		if(knjiga == null) {
+			throw new RuntimeException("Knjiga ne sme biti null!");
+		}
+		if(knjige.contains(knjiga)) {
+			int indeks = knjige.indexOf(knjiga);
+			int kolicina = knjige.get(indeks).getKolicina();
+			try {
+				knjige.get(indeks).setKolicina(kolicina + knjiga.getKolicina());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			knjige.add(knjiga);
+		}
+		
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		
+		if(knjiga == null) {
+			throw new RuntimeException("Knjiga ne sme biti null!");
+		}
+		if(knjige.contains(knjiga)) {
+			int indeks = knjige.indexOf(knjiga);
+			int kolicina = knjige.get(indeks).getKolicina();
+			try {
+				if(kolicina>knjiga.getKolicina()) {
+					knjige.get(indeks).setKolicina(kolicina - knjiga.getKolicina());
+				} else {
+					knjige.remove(knjiga);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			throw new RuntimeException("Knjiga ne postoji u magacinu pa se ne moze obrisati!");
+		}
 	}
 
 	@Override
 	public Knjiga pronadjiIVratiKnjigu(int sifra) {
-		// TODO Auto-generated method stub
+		
+		for (Knjiga knjiga : knjige) {
+			if(sifra== knjiga.getSifra()) {
+				return knjiga;
+			}
+		}
+		
 		return null;
 	}
 
 	@Override
 	public void dodajKancelarijskiMaterijal(KancelarijskiMaterijal materijal) {
-		// TODO Auto-generated method stub
-
+		
+		if(materijal == null) {
+			throw new RuntimeException("Kancelarijski materijal ne sme biti null!");
+		}
+		if(kancelarijskiMaterijali.contains(materijal)) {
+			int indeks = kancelarijskiMaterijali.indexOf(materijal);
+			int kolicina = kancelarijskiMaterijali.get(indeks).getKolicina();
+			try {
+				kancelarijskiMaterijali.get(indeks).setKolicina(kolicina + materijal.getKolicina());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			kancelarijskiMaterijali.add(materijal);
+		}
 	}
 
 	@Override
 	public void obrisiKancelarijskiMaterijal(KancelarijskiMaterijal materijal) {
-		// TODO Auto-generated method stub
-
+		
+		if(materijal == null) {
+			throw new RuntimeException("Kancelarijski materijal ne sme biti null!");
+		}
+		if(kancelarijskiMaterijali.contains(materijal)) {
+			int indeks = kancelarijskiMaterijali.indexOf(materijal);
+			int kolicina = kancelarijskiMaterijali.get(indeks).getKolicina();
+			try {
+				if(kolicina>materijal.getKolicina()) {
+					kancelarijskiMaterijali.get(indeks).setKolicina(kolicina - materijal.getKolicina());
+				} else {
+					kancelarijskiMaterijali.remove(materijal);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			throw new RuntimeException("Kancelarijski materijal ne postoji u magacinu pa se ne moze obrisati!");
+		}
 	}
 
 	@Override
 	public KancelarijskiMaterijal pronadjiIVratiKancMaterijal(int sifra) {
-		// TODO Auto-generated method stub
+		
+		for (KancelarijskiMaterijal materijal : kancelarijskiMaterijali) {
+			if(sifra == materijal.getSifra()) {
+				return materijal;
+			}
+		}
+		
 		return null;
 	}
 
 	@Override
 	public void dodajKucnuHemiju(KucnaHemija hemija) {
-		// TODO Auto-generated method stub
-
+		
+		if(hemija == null) {
+			throw new RuntimeException("Kucna hemija ne sme biti null!");
+		}
+		if(kucneHemije.contains(hemija)) {
+			int indeks = kucneHemije.indexOf(hemija);
+			int kolicina = kucneHemije.get(indeks).getKolicina();
+			try {
+				kucneHemije.get(indeks).setKolicina(kolicina + hemija.getKolicina());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			kucneHemije.add(hemija);
+		}
 	}
 
 	@Override
 	public void obrisiKucnuHemiju(KucnaHemija hemija) {
-		// TODO Auto-generated method stub
 
+		if(hemija == null) {
+			throw new RuntimeException("Kucna hemija ne sme biti null!");
+		}
+		if(kucneHemije.contains(hemija)) {
+			int indeks = kucneHemije.indexOf(hemija);
+			int kolicina = kucneHemije.get(indeks).getKolicina();
+			try {
+				if(kolicina>hemija.getKolicina()) {
+					kucneHemije.get(indeks).setKolicina(kolicina - hemija.getKolicina());
+				} else {
+					kucneHemije.remove(hemija);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			throw new RuntimeException("Kucna hemija ne postoji u magacinu pa se ne moze obrisati!");
+		}
 	}
 
 	@Override
 	public KucnaHemija pronadjiIVratiKucnuHemiju(int sifra) {
-		// TODO Auto-generated method stub
+		
+		for (KucnaHemija hemija : kucneHemije) {
+			if(sifra == hemija.getSifra()) {
+				return hemija;
+			}
+		}
+		
 		return null;
 	}
 
